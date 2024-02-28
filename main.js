2110c8a2365b5c4453ab2c896bc228a5ceb2c5c2
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-02-28 13:00:35 +0500

    Assignment 2

---
+++ main.js
@@ -0,0 +1,28 @@
+"use strict";
+// let student : {
+// myName : string
+// Age : number
+// address : string | number
+// are_youpakistani : boolean
+// }
+// student = {
+//  / myName :"shahmeer",
+// Age : 20 ,
+//address : "xyz karachi pakistan",
+// are_youpakistani : true 
+// }
+// console.log(student["myName"]); 
+//let sunday = "weekend day"
+// console.log(sunday)
+// this const and not change vale
+//const username = "shahmeer ";
+//console.log(username); 
+let personal;
+personal = {
+    account: "manger",
+    username: "Taha",
+    password: 1200034,
+};
+console.log(personal["account"]);
+console.log(personal["password"]);
+console.log(personal["username"]);
---
+++ main.ts
@@ -0,0 +1,65 @@
+// let student : {
+    // myName : string
+    // Age : number
+    // address : string | number
+    // are_youpakistani : boolean
+// }
+ 
+  // student = {
+  //  / myName :"shahmeer",
+    // Age : 20 ,
+    //address : "xyz karachi pakistan",
+    // are_youpakistani : true 
+// }
+
+// console.log(student["myName"]); 
+
+//let sunday = "weekend day"
+ // console.log(sunday)
+    
+// this const and not change vale
+  
+
+//const username = "shahmeer ";
+ 
+
+//console.log(username); 
+
+let personal :{
+   
+   account : string ;
+
+  username : string ;
+
+   password : number ;
+
+}
+personal = {
+
+  account : "manger",
+   
+  username : "Taha",
+
+  password : 1200034 ,
+} 
+const country_code : number = 92 
+
+console.log(personal["account"]);
+console.log(personal["password"]);
+console.log(personal["username"]);
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+ 
